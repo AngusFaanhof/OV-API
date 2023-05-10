13aec53da94d9c1f266dd6fe5452cac8a7d6bedc
AngusFaanhof <github@adfpm.anonaddy.com>
2023-05-10 13:13:52 +0200

    Fixed headles bug

--- script.js
+++ script.js
@@ -2,39 +2,44 @@ const puppeteer = require('puppeteer');
 
 async function getData(from, to) {
 	const browser = await puppeteer.launch({
-		headless: false,
+		// headless: false,
+		headless: "new",
+		args: ['--disable-web-security', '--disable-features=IsolateOrigins', ' --disable-site-isolation-trials'],
 	});
 
 	const page = await browser.newPage();
 
 	await page.goto('https://9292.nl', { waitUntil: 'networkidle2' });
 
-
-	// Accept cookies. TODO: Make it work in headless mode
+	// Accept cookies
 	await page.waitForSelector('iframe');
-	await page.keyboard.down('Shift');
-	await page.keyboard.press('Tab');
-	await page.keyboard.up('Shift');
-	await page.keyboard.press("Enter");
+	const frame = page.frames().find(frame => frame.url().includes('consent-tool'));
 
+	const acceptButton = await frame.$('button#save');
+	acceptButton.click();
 
 	async function results() {
 		await page.waitForSelector('input#van');
+		await new Promise(r => setTimeout(r, 500)); // Let page fully load
+
 		await page.type('input#van', from);
+
+		await page.waitForSelector('input#naar');
 		await page.type('input#naar', to);
 
 		await page.click('button[type="submit"]');
 
+		// Next page in sequence
 		await page.waitForSelector('#from-text-again')
 		await page.click('button[type="submit"]');
 
 		await page.waitForSelector('#journeyTabList');
-		const contents = await page.$eval('#journeyTabList', el => el.textContent);
+		const contents = await page.$eval('#journeyTabList', el => el.innerHTML);
+
 		console.log(contents);
 	}
 
 	results();
-	// console.log(results());
 }
 
-getData('Zaandam', 'Vrije Universiteit Amsterdam, Amsterdam');
+getData('Zaandam', 'Metro Amstelveenseweg');
