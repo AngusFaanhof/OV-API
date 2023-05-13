c2d32265740b4e41280d85d8fd63d0f3442f18a6
AngusFaanhof <github@adfpm.anonaddy.com>
2023-05-13 17:48:36 +0200

    small code reformat

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ async function navigateToDetails(page, from, to, date) {
 	// Next page in sequence
 	await page.waitForSelector('#from-text-again')
 	await page.click('button[type="submit"]');
-
-	await page.waitForSelector('#journeyTabList');
 }
 
 async function extractAPIData(page) {
+	await page.waitForSelector('#journeyTabList');
+
 	//format to steps array
 	let handles = await page.$$('div.active ul li');
 	let steps = [];
@@ -29,18 +29,14 @@ async function extractAPIData(page) {
 		const handle = handles[i];
 		const response = {}
 
-		// transport
 		const transportType = await handle.$eval('strong', (el) => el.innerText);
 		response.transport = transportType;
 
-		// direction
 		if (transportType != 'Lopen')
 			response.direction = await handle.$eval('.text-muted.small', (el) => el.innerText);
 
-		// times
 		response.times = await handle.$$eval('.timeline-time span', els => els.map(el => el.innerText));;
 
-		// from / to
 		const allStations = await handle.$$eval('.leg-link a', els => {
 			return els.map(el => el.innerText);
 		});
@@ -48,14 +44,13 @@ async function extractAPIData(page) {
 		response.from = i == 0 ? allStations[0] : steps[i-1].to;
 		response.to = i == 0 ? allStations[1] : allStations[0];
 
-		// Add track logic
 		if (transportType.startsWith('NS')) {
 			const elementTexts = await handle.$$eval('.timeline-type > span', els => {
 				return els.map(el => el.innerText);
 			});
 
-			response.from += ', ' + elementTexts[2];
-			response.to += ', ' + elementTexts[4];
+			response.from += ' - ' + elementTexts[2];
+			response.to += ' - ' + elementTexts[4];
 		}
 
 		steps.push(response);
@@ -68,8 +63,7 @@ async function extractAPIData(page) {
 	}
 }
 
-
-async function getData(from, to, date) {
+async function getAPIData(from, to, date) {
 	const browser = await puppeteer.launch({
 		// headless: false,
 		headless: "new",
@@ -77,21 +71,23 @@ async function getData(from, to, date) {
 	});
 
 	const page = await browser.newPage();
-
 	await page.goto('https://9292.nl', { waitUntil: 'networkidle2' });
 
 	// Accept cookies
 	await page.waitForSelector('iframe');
 	const frame = page.frames().find(frame => frame.url().includes('consent-tool'));
-
 	const acceptButton = await frame.$('button#save');
 	acceptButton.click();
 
 	await navigateToDetails(page, from, to, date);
 	const apiResponse = await extractAPIData(page);
-	console.log(apiResponse);
 	// const jsonApiResponse = JSON.stringify(apiResponse);
+	console.log(apiResponse);
+	return apiResponse;
 }
 
+const from = 'Zaandam';
+const to = 'Metro Amstelveenseweg';
 const date = new Date("2023-05-10T12:00:00+01:00");
-getData('Zaandam', 'Metro Amstelveenseweg', date);
+
+getAPIData(from, to, date);
