16d7521d78944e1148444a4361ffcf6c8ad7ef5b
AngusFaanhof <github@adfpm.anonaddy.com>
2023-05-21 10:38:04 +0200

    added timestamp support

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.get('/:from/:to', async function (req, res) {
 	res.json(response);
 });
 
+app.get('/:from/:to/:timestamp', async function (req, res) {
+	const from = req.params.from;
+	const to = req.params.to;
+	const date = new Date(req.params.timestamp * 1000);
+
+	const response = await getAPIData(from, to, date);
+
+	res.json(response);
+});
+
 app.listen(3000, () => {
 	console.log("Server running on port http://localhost:3000");
 });
